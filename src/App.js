70cb6af3df84116dae036f97b7a7491cfceb950e
Hamza <hamzayousuf175@gmail.com>
2020-08-26 16:40:25 +0500

    Added all styling and everything is working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   //STATE
   const [imgPreviewS, setimgPreviewS] = useState();
   const [modalS, setmodalS] = useState(null);
-  const [classifiedResultS, setclassifiedResultS] = useState([]);
+  const [classifiedResultS, setclassifiedResultS] = useState();
   const [isLoadingS, setisLoadingS] = useState(false);
 
   //Ref of html tag
@@ -29,6 +29,9 @@ const App = () => {
   //img handler also set img preview
   const imagePickHandler = (event) => {
     const img = event.target.files[0];
+
+    if (!img) return;
+
     const imgURL = URL.createObjectURL(img);
     console.log(img);
     setimgPreviewS(imgURL);
@@ -53,7 +56,6 @@ const App = () => {
 
       {!isLoadingS && (
         <div className="container ">
-          
           {/**STEP 1 LOAD MODEL */}
           {!modalS && (
             <div className="row">
@@ -72,54 +74,58 @@ const App = () => {
           )}
 
           {/**STEP 2 Chosse file */}
-          <div className="row">
-            <div className="col-xs-4 input-group ">
-              <span className="input-group-addon ">
-                <i className="glyphicon glyphicon-upload "></i>
-              </span>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={imagePickHandler}
-                ref={fileTagRef}
-                className="form-control"
-              />
-            </div>
+          {modalS && (
+            <div className="row">
+              <div className="col-xs-4 input-group " style={{ margin: "15px" }}>
+                <span className="input-group-addon ">
+                  <i className="glyphicon glyphicon-upload "></i>
+                </span>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={imagePickHandler}
+                  ref={fileTagRef}
+                  className="form-control"
+                />
+              </div>
 
-            {/**IMGE PREIVEW SECTION */}
-            <img
-              className="img-thumbnail"
-              src={imgPreviewS}
-              ref={imgTagRef}
-              alt="img preview"
-              width="250px"
-              height="200px"
-            />
-            <button
-              className="btn btn-info btn-lg"
-              style={{ marginLeft: "15px" }}
-              onClick={classifyingImg}
-            >
-              <i className="glyphicon glyphicon-cog"></i> Classified
-            </button>
-          </div>
+              {imgPreviewS && (
+                <React.Fragment>
+                  {/**IMGE PREIVEW SECTION */}
+                  <img
+                    className="img-thumbnail"
+                    src={imgPreviewS}
+                    ref={imgTagRef}
+                    alt="img preview"
+                    width="250px"
+                    height="200px"
+                  />
+                  <button
+                    className="btn btn-info btn-lg"
+                    style={{ marginLeft: "15px" }}
+                    onClick={classifyingImg}
+                  >
+                    <i className="glyphicon glyphicon-cog"></i> Classified
+                  </button>
+                </React.Fragment>
+              )}
+            </div>
+          )}
 
           {/**STEP 3 LOAD MODEL */}
           {classifiedResultS && (
-            <div className="row">
-              <div className="panel panel-info">
-                {classifiedResultS.map((singlePrediction, index) => (
-                  <React.Fragment key={index}>
-                    <div className="panel-heading">
-                      Classified as {singlePrediction.className}
-                    </div>
-                    <div className="panel-body">
-                      Percentage:%{" "}
-                      {(singlePrediction.probability * 100).toFixed(2)}
-                    </div>
-                  </React.Fragment>
-                ))}
-              </div>
+            <div className="row" style={{ margin: "15px" }}>
+              {classifiedResultS.map((singlePrediction, index) => (
+                <div className="panel panel-info" key={index} style={{ marginBottom: "15px" }}>
+                  <div className="panel-heading">
+                    Classified as {singlePrediction.className}
+                  </div>
+                  <div className="panel-body" >
+                    Percentage:%
+                    {(singlePrediction.probability * 100).toFixed(2)}
+                  </div>
+                </div>
+              ))}
             </div>
           )}
         </div>
